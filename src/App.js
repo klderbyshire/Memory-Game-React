ee13f6b78b9c1ba57fe7b9971023e00f684037ae
karen derbyshire <karenderbyshire5@gmail.com>
2022-08-11 14:58:15 +0100

    setTimeout function added to wait 1 second before flipping unmatched cards

--- src/App.js
+++ src/App.js
@@ -48,7 +48,7 @@ function App() {
         });
         resetTurn();
       } else {
-        resetTurn();
+        setTimeout(() => resetTurn(), 1000);
       }
     }
   }, [choiceOne, choiceTwo]);
