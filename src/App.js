5fa3ea49f08cc8d8e94b331e8a414a1c94113032
karen derbyshire <karenderbyshire5@gmail.com>
2022-08-11 16:52:01 +0100

    Finishing touches and 2nd useEffect added

--- src/App.js
+++ src/App.js
@@ -25,6 +25,8 @@ function App() {
       .sort(() => Math.random() - 0.5)
       .map((card) => ({ ...card, id: Math.random() }));
 
+    setChoiceOne(null);
+    setChoiceTwo(null);
     setCards(shuffledCards);
     setTurns(0);
   };
@@ -63,6 +65,11 @@ function App() {
     setDisabled(false);
   };
 
+  //start a new game automatically
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
@@ -79,6 +86,7 @@ function App() {
           />
         ))}
       </div>
+      <p>Turns: {turns}</p>
     </div>
   );
 }
