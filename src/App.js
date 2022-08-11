4207de8cf6031030fa6d4f384197372c5d959123
karen derbyshire <karenderbyshire5@gmail.com>
2022-08-11 11:32:20 +0100

    Creat useEffect hook to check if two cards match

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import "./App.css";
 import SingleCard from "./components/SingleCard/SingleCard.js";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const cardImages = [
-  { "src": "/img/helmet-1.png" },
-  { "src": "/img/potion-1.png" },
-  { "src": "/img/ring-1.png" },
-  { "src": "/img/scroll-1.png" },
-  { "src": "/img/shield-1.png" },
-  { "src": "/img/sword-1.png" },
+  { src: "/img/helmet-1.png" },
+  { src: "/img/potion-1.png" },
+  { src: "/img/ring-1.png" },
+  { src: "/img/scroll-1.png" },
+  { src: "/img/shield-1.png" },
+  { src: "/img/sword-1.png" },
 ];
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
-  const [choiceOne, setChoiceOne] = useState(null)
-  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // shuffle cards
   //duplicate card images using spread
@@ -27,28 +27,47 @@ function App() {
     setCards(shuffledCards);
     setTurns(0);
   };
-  
+
   // handle a choice
   const handleChoice = (card) => {
-choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
-  }
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  };
 
   //compare two selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+      console.log("Its a match!");
+      resetTurn()
+    } else {
+      console.log("Its not a match!");
+      resetTurn()
+    }
+  }
+    // if (choiceOne !== choiceTwo) {
+     
+      
+    // }
+  }, [choiceOne, choiceTwo]);
+
+  // reset choices and increase turn
+const resetTurn = () => {
+  setChoiceOne(null)
+  setChoiceTwo(null)
+  setTurns(prevTurns => prevTurns + 1)
+}
+
 
   return (
     <div className="App">
       <h1>Magic Match</h1>
       <button onClick={shuffleCards}>New Game</button>
-      
+
       <div className="card-grid">
-        {cards.map(card => (
-      <SingleCard 
-      key={card.id} 
-      card={card} 
-      handleChoice={handleChoice}
-      />
+        {cards.map((card) => (
+          <SingleCard key={card.id} card={card} handleChoice={handleChoice} />
         ))}
-    </div>
+      </div>
     </div>
   );
 }
